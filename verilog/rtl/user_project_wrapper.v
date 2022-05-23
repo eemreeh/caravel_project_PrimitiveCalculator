c37710397e98d15f2a6e38702dad77955b22405f
eemreeh <emrehep@gmail.com>
2022-05-23 21:32:11 +0300

    commit 3

--- verilog/rtl/user_project_wrapper.v
+++ verilog/rtl/user_project_wrapper.v
@@ -82,40 +82,27 @@ module user_project_wrapper #(
 /* User project is instantiated  here   */
 /*--------------------------------------*/
 
-user_proj_example mprj (
+PrimitiveCalculator PrimitiveCalculator (
 `ifdef USE_POWER_PINS
 	.vccd1(vccd1),	// User area 1 1.8V power
 	.vssd1(vssd1),	// User area 1 digital ground
 `endif
+    .clk(wb_clk_i),
+    .rst(la_data_in[0]),
 
-    .wb_clk_i(wb_clk_i),
-    .wb_rst_i(wb_rst_i),
+    .select(io_in[8]),
+    .restart(io_in[9]),
+    .rotary_a(io_in[10]),
+    .rotary_b(io_in[11]),
+    .seven_segment_out(io_out[18:12]),
+    .seven_segment_digit(io_out[19]),
+    .led_flag(io_out[20]),
+    .sync(io_out[21]),
+    .io_oeb(io_oeb[21:12])
+	
 
-    // MGMT SoC Wishbone Slave
 
-    .wbs_cyc_i(wbs_cyc_i),
-    .wbs_stb_i(wbs_stb_i),
-    .wbs_we_i(wbs_we_i),
-    .wbs_sel_i(wbs_sel_i),
-    .wbs_adr_i(wbs_adr_i),
-    .wbs_dat_i(wbs_dat_i),
-    .wbs_ack_o(wbs_ack_o),
-    .wbs_dat_o(wbs_dat_o),
 
-    // Logic Analyzer
-
-    .la_data_in(la_data_in),
-    .la_data_out(la_data_out),
-    .la_oenb (la_oenb),
-
-    // IO Pads
-
-    .io_in (io_in),
-    .io_out(io_out),
-    .io_oeb(io_oeb),
-
-    // IRQ
-    .irq(user_irq)
 );
 
 endmodule	// user_project_wrapper
